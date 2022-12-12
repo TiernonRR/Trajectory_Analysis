9eb96b15ee4147466f9d520fac3ec9141a1eaf5e
TiernonRR <tiernon.riesenmy@gmail.com>
2022-12-12 16:10:51 -0500

    update combineGAM

--- combineGAM.R
+++ combineGAM.R
@@ -1,27 +1,22 @@
 library(SingleCellExperiment)
-data_prefix <- "IsoHuB"
-path <- paste0("/home/tr455/project/temp/", data_prefix, "_gam/", data_prefix)
+library(tradeSeq)
 
-# Missing 1501 and 501 - CMC
-# missing 1 = krie
+## Use this file to combine result of dead simple queue job
 
-lst <- c("501", "1001", "1501", "2001", "2501", "3001", "3501", "4001", 
-         "4501", "5001", "5501", "6001", "6501", "7001", "7039")
+data_prefix <- "DevBrain"
 
-combined_GAM <- readRDS(paste0(path, "1_gam_it.rds"))
-for (i in 1:length(lst)){
-    l <- lst[i]
-    print(l)
-    
-    gam <- readRDS(paste0(path, l, "_gam_it.rds"))
-    print(dim(gam))
-    print(dim(combined_GAM))
-    combined_GAM <- rbind(combined_GAM, gam)
+## Path to directory containing result of dead simple queue jobs
+path <- paste0("/home/tr455/project/final/subset_tmp/", data_prefix, "_gam/")
+
+files <- dir(path = path)
+combined_GAM <- readRDS(paste0(path, files[1]))
+
+## Iterate over files in directory and combine
+for(i in 2:length(files)) {
+  gam <- readRDS(paste0(path, files[i]))
+  combined_GAM <- rbind(combined_GAM, gam)
 }
 
-saveRDS(combined_GAM, paste0("/home/tr455/project/", data_prefix, "_gam_it.rds"))
-temp <- readRDS(paste0("/home/tr455/project/", data_prefix, "_gam_it.rds"))
+## Save GAM to desired directory
+saveRDS(combined_GAM, paste0("/home/tr455/project/final/subset/", data_prefix, "_gam_final.rds"))
 
-gam1 <- readRDS(paste0(path, "501", "_gam_it.rds"))
-
-final <- rbind(temp, gam1)
